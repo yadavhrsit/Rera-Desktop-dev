26bbfd46ab6c133b0df8ac93179804bdd61f9c66
Harshit Yadav <78172595+yadavhrsit@users.noreply.github.com>
2023-07-14 20:29:18 +0530

    Update StaffManagementPage.js

--- src/pages/StaffManagementPage.js
+++ src/pages/StaffManagementPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { filter } from 'lodash';
 import { Helmet } from 'react-helmet-async';
 import {
@@ -14,6 +14,8 @@ import {
     Typography,
     TableContainer,
     TablePagination,
+    TextField,
+    Modal,
 } from '@mui/material';
 
 import Iconify from '../components/iconify';
@@ -23,37 +25,7 @@ import { UserListHead, UserListToolbar } from '../sections/@dashboard/user';
 
 import STAFFLIST from '../_mock/staff';
 
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-
-function getComparator(order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function applySortFilter(array, comparator, query) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) return order;
-        return a[1] - b[1];
-    });
-    if (query) {
-        return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-    }
-    return stabilizedThis.map((el) => el[0]);
-}
-
 function StaffManagementPage() {
-
     const [page, setPage] = useState(0);
     const [order, setOrder] = useState('asc');
     const [selected, setSelected] = useState([]);
@@ -61,7 +33,50 @@ function StaffManagementPage() {
     const [filterName, setFilterName] = useState('');
     const [rowsPerPage, setRowsPerPage] = useState(5);
 
+    // ----------------------------------------------------------------------
 
+    function descendingComparator(a, b, orderBy) {
+        if (b[orderBy] < a[orderBy]) {
+            return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+            return 1;
+        }
+        return 0;
+    }
+
+    function getComparator(order, orderBy) {
+        return order === 'desc'
+            ? (a, b) => descendingComparator(a, b, orderBy)
+            : (a, b) => -descendingComparator(a, b, orderBy);
+    }
+
+    function applySortFilter(array, comparator, query) {
+        const stabilizedThis = array.map((el, index) => [el, index]);
+        stabilizedThis.sort((a, b) => {
+            const order = comparator(a[0], b[0]);
+            if (order !== 0) return order;
+            return a[1] - b[1];
+        });
+        if (query) {
+            return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+        }
+        return stabilizedThis.map((el) => el[0]);
+    }
+
+    // Modal state
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [mobileNumber, setMobileNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    // Validation state
+    const [isFormValid, setIsFormValid] = useState(true);
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
+    const [isConfirmPasswordValid, setIsConfirmPasswordValid] = useState(true);
 
     const handleRequestSort = (event, property) => {
         const isAsc = orderBy === property && order === 'asc';
@@ -92,6 +107,73 @@ function StaffManagementPage() {
         setFilterName(event.target.value);
     };
 
+    const handleModalOpen = () => {
+        setIsModalOpen(true);
+        setFullName('');
+        setEmail('');
+        setMobileNumber('');
+        setPassword('');
+        setConfirmPassword('');
+        setIsFormValid(true);
+        setIsEmailValid(true);
+        setIsPasswordValid(true);
+        setIsConfirmPasswordValid(true);
+    };
+
+    const handleModalClose = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleFullNameChange = (event) => {
+        setFullName(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handleMobileNumberChange = (event) => {
+        setMobileNumber(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleConfirmPasswordChange = (event) => {
+        setConfirmPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Validate form fields
+        let isValid = true;
+
+        if (!fullName || !email || !mobileNumber || !password || !confirmPassword) {
+            setIsFormValid(false);
+            isValid = false;
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!email.match(emailRegex)) {
+            setIsEmailValid(false);
+            isValid = false;
+        }
+
+        if (password.length < 6 || password !== confirmPassword) {
+            setIsPasswordValid(false);
+            setIsConfirmPasswordValid(false);
+            isValid = false;
+        }
+
+        if (isValid) {
+            // Perform submit logic here
+            console.log('Form submitted:', { fullName, email, mobileNumber, password });
+            handleModalClose();
+        }
+    };
+
     const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - STAFFLIST.length) : 0;
 
     const filteredUsers = applySortFilter(STAFFLIST, getComparator(order, orderBy), filterName);
@@ -115,7 +197,7 @@ function StaffManagementPage() {
                     <Typography variant="h4" gutterBottom>
                         Staff Management
                     </Typography>
-                    <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
+                    <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} onClick={handleModalOpen}>
                         Add New Staff
                     </Button>
                 </Stack>
@@ -207,8 +289,85 @@ function StaffManagementPage() {
                     />
                 </Card>
             </Container>
+
+            {/* Add New Staff Modal */}
+            <Modal open={isModalOpen} onClose={handleModalClose}>
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        backgroundColor: 'white',
+                        padding: '2rem',
+                        outline: 'none',
+                    }}
+                >
+                    <Typography variant="h5" gutterBottom>
+                        Add New Staff
+                    </Typography>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label="Full Name"
+                            value={fullName}
+                            onChange={handleFullNameChange}
+                            error={!isFormValid && !fullName}
+                            helperText={!isFormValid && !fullName && 'Full Name is required'}
+                            margin="normal"
+                            fullWidth
+                        />
+                        <TextField
+                            label="Email"
+                            type="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            error={!isFormValid && (!email || !isEmailValid)}
+                            helperText={!isFormValid && (!email ? 'Email is required' : 'Invalid email format')}
+                            margin="normal"
+                            fullWidth
+                        />
+                        <TextField
+                            label="Mobile Number"
+                            value={mobileNumber}
+                            onChange={handleMobileNumberChange}
+                            error={!isFormValid && !mobileNumber}
+                            helperText={!isFormValid && !mobileNumber && 'Mobile Number is required'}
+                            margin="normal"
+                            fullWidth
+                        />
+                        <TextField
+                            label="Password"
+                            type="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            error={!isFormValid && (!password || !isPasswordValid)}
+                            helperText={!isFormValid && (!password ? 'Password is required' : 'Password should be at least 6 characters long')}
+                            margin="normal"
+                            fullWidth
+                        />
+                        <TextField
+                            label="Confirm Password"
+                            type="password"
+                            value={confirmPassword}
+                            onChange={handleConfirmPasswordChange}
+                            error={!isFormValid && (!confirmPassword || !isConfirmPasswordValid)}
+                            helperText={
+                                !isFormValid && (!confirmPassword ? 'Confirm Password is required' : "Passwords don't match")
+                            }
+                            margin="normal"
+                            fullWidth
+                        />
+                        <Button variant="contained" color="primary" type="submit" style={{ marginRight: '1rem' }}>
+                            Add
+                        </Button>
+                        <Button variant="contained" onClick={handleModalClose}>
+                            Cancel
+                        </Button>
+                    </form>
+                </div>
+            </Modal>
         </>
-    )
+    );
 }
 
-export default StaffManagementPage
+export default StaffManagementPage;
